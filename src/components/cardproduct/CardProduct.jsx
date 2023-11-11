427b384fe6e88a9c88cb005209b7f64d46ca5ae6
nachofiliberti <j.filiberti@itecriocuarto.org.ar>
2023-11-11 20:20:29 -0300

    yes

--- src/components/cardproduct/CardProduct.jsx
+++ src/components/cardproduct/CardProduct.jsx
@@ -1,3 +1,5 @@
+import intelCeleron from '../../assets/images/products/procesadores/intelCeleron.jpg'
+
 export default function CardProduct() {
     return (
         <div className="col-span-3">
@@ -67,7 +69,7 @@ export default function CardProduct() {
 
                 <div className="bg-white shadow rounded overflow-hidden group">
                     <div className="relative">
-                        <img src="../assets/images/products/product2.jpg" alt="product 1" className="w-full" />
+                        <img src={intelCeleron} alt="product 1" className="w-full" />
                         <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center 
                         justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
                             <a href="#"
@@ -84,9 +86,7 @@ export default function CardProduct() {
                     </div>
                     <div className="pt-4 pb-3 px-4">
                         <a href="#">
-                            <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">
-                                Guyer
-                                Chair</h4>
+                            <h4 className="uppercase font-medium text-xl mb-2 text-gray-800 hover:text-primary transition">Intel celeron - G4900 - 3.10ghz</h4>
                         </a>
                         <div className="flex items-baseline mb-1 space-x-2">
                             <p className="text-xl text-primary font-semibold">$45.00</p>
--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -6,7 +6,7 @@ export default function Categories() {
             <div className="grid grid-cols-3 gap-3">
                 <div className="relative rounded-sm overflow-hidden group">
                     <img src="src/assets/images/category/category-1.jpg" alt="Procesadores" className="w-full" />
-                    <a href="#"
+                    <a href="/shop/procesadores"
                         className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-xl text-white font-roboto font-medium group-hover:bg-opacity-60 transition">
                         Procesadores
                     </a>
