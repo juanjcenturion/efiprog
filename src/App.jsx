146531ea70c888b85f67af58e5650a1ca8607987
nachofiliberti <j.filiberti@itecriocuarto.org.ar>
2023-11-11 18:51:53 -0300

    agregue 2 componentes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/header/Header'
 import NavBar from './components/navbar/NavBar'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
+import ShopP from './pages/ShopP'
 
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/contact' element={<Contact/>}/>
-          
+          <Route path='/shop/procesadores' element={<ShopP/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ShopP.jsx
@@ -0,0 +1,11 @@
+import BreadCrumb from "../components/breadcrumb/BreadCrumb";
+import CardProduct from "../components/cardproduct/CardProduct";
+
+export default function ShopP () {
+    return (
+        <div>
+            <BreadCrumb/>
+            <CardProduct/>
+        </div>
+    )
+}
